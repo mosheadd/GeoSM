417458bdde53e6a424057c3f59edc57664cece50
mosheadd <rost1234567890qgp@gmail.com>
2022-03-24 17:52:36 +0300

    Site Launch

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from flask import Flask
+
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+app.config['SECRET KEY'] = 'mosheadd176440'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
 def main():
-    pass
+    app.run()
 
 
 if __name__ == "__main__":
