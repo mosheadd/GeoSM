4580cc936d4e929727799767d96d0094b0a83ed8
mosheadd <rost1234567890qgp@gmail.com>
2022-04-10 11:05:37 +0300

    Update databases

---
+++ databases/users.py
@@ -0,0 +1,21 @@
+import sqlalchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase, UserMixin):
+    __tablename__ = "users"
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    login = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
