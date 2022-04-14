0fbe4399c73aed801af58f4f0ab9fde595818f83
LouisVitok <93275064+LouisVitok@users.noreply.github.com>
2022-04-14 16:30:05 +0300

    snake_3

--- snace.py
+++ snace.py
@@ -1,5 +1,8 @@
 import pygame
 from random import randrange
+import sys
+
+from pygame.examples.aliens import load_image
 
 RES = 1000
 SIZE = 40
@@ -11,19 +14,86 @@ lenght = 1
 snake = [(x, y)]
 dx, dy = 0, 0
 score = 0
-fps = 4
+fps = 8
+hs = [0]
 
 pygame.init()
 sc = pygame.display.set_mode([RES, RES])
 clock = pygame.time.Clock()
 font_score = pygame.font.SysFont('Arial', 26, bold=True)
 font_end = pygame.font.SysFont('Arial', 69, bold=True)
+font_hs = pygame.font.SysFont('Arial', 26, bold=True)
 img = pygame.image.load('look.com.ua-256556.jpg').convert()
 
-while True:
+
+def start_screen(width, height):
+    intro = ['Выберите цвет змейки', 'Зеленый', 'Синий', 'Желтый']
+    fonts = []
+    background = pygame.transform.scale(load_image('look.com.ua-256556.jpg'), (width, height))
+    sc.blit(background, (0, 0))
+    y = 5
+    for line in intro:
+        if line == 'Зеленый':
+            s = pygame.font.Font(None, 70).render(line, True, (0, 250, 0))
+        elif line == 'Синий':
+            s = pygame.font.Font(None, 70).render(line, True, (0, 0, 250))
+        elif line == 'Желтый':
+            s = pygame.font.Font(None, 70).render(line, True, 'yellow')
+        else:
+            s = pygame.font.Font(None, 70).render(line, True, (250, 250, 210))
+        rect = s.get_rect()
+        rect.x = width // 2 - rect.w // 2
+        rect.y = y
+        y += height // 4
+        fonts.append(s)
+        sc.blit(s, (rect.x, rect.y))
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+                return
+            if event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                y = 5
+                for i in range(len(fonts)):
+                    rect = fonts[i].get_rect()
+                    rect.x = width // 2 - rect.w // 2
+                    rect.y = y
+                    if rect.collidepoint(pygame.mouse.get_pos()) and i != 0:
+                        if intro[i] == 'Зеленый':
+                            return 1
+                        if intro[i] == 'Синий':
+                            return 2
+                        if intro[i] == 'Желтый':
+                            return 3
+                    y += height // 4
+        # screen.blit(background, (0, 0))
+        for i in range(len(intro)):
+            if fonts[i].get_rect().collidepoint(pygame.mouse.get_pos()):
+                color = (255, 10, 10)
+            else:
+                color = (250, 250, 210)
+            rect = fonts[i].get_rect()
+            x = width // 2 - rect.w // 2
+            sc.blit(fonts[i], (x, y))
+            pygame.display.flip()
+            y += height // 4
+        pygame.display.flip()
+        clock.tick(fps)
+
+
+def game(qwer):
+    global apple, score, snake, x, dx, y, dy, lenght, fps, dirs, hs
     sc.blit(img, (0, 0))
     # drawing snake
-    [(pygame.draw.rect(sc, pygame.Color('green'), (i, j, SIZE - 2, SIZE - 2))) for i, j in snake]
+    if qwer == 1:
+        col = 'green'
+    if qwer == 2:
+        col = 'blue'
+    if qwer == 3:
+        col = 'yellow'
+    [(pygame.draw.rect(sc, pygame.Color(col), (i, j, SIZE - 2, SIZE - 2))) for i, j in snake]
     pygame.draw.rect(sc, pygame.Color('red'), (*apple, SIZE, SIZE))
     # show score
     render_score = font_score.render(f'Score: {score}', 1, pygame.Color('orange'))
@@ -42,8 +112,14 @@ while True:
     # game over
     if x < 0 or x > RES - SIZE or y < 0 or y > RES - SIZE or len(snake) != len(set(snake)):
         while True:
-            render_end = font_end.render('GAME OVER', 1, pygame.Color('orange'))
+            if score > hs[-1]:
+                hs.append(score)
+            render_end = font_end.render('GAME OVER', 1, pygame.Color('red'))
+            render_score = font_score.render(f'Score: {str(score)}', 1, pygame.Color('blue'))
+            render_hs = font_hs.render(f'Hight score: {str(hs[-1])}', 1, pygame.Color('blue'))
             sc.blit(render_end, (RES // 2 - 200, RES // 3))
+            sc.blit(render_score, (RES // 2 - 200, RES // 4))
+            sc.blit(render_hs, (RES // 2 + 50, RES // 4))
             pygame.display.flip()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -73,3 +149,9 @@ while True:
     if key[pygame.K_d] and dirs['D']:
         dx, dy = 1, 0
         dirs = {'W': True, 'A': False, 'S': True, 'D': True}
+
+while True:
+    diff = start_screen(RES, RES)
+    if diff == 1 or diff == 2 or diff == 3:
+        while True:
+            game(diff)
