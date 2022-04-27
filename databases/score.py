13d2fcfbd978dc63ee88ac6bec40421c2242f0c1
mosheadd <rost1234567890qgp@gmail.com>
2022-04-27 16:22:29 +0300

    Update score.py

--- databases/score.py
+++ databases/score.py
@@ -8,4 +8,5 @@ class Score(SqlAlchemyBase):
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     user_id = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    game = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     score = sqlalchemy.Column(sqlalchemy.String, nullable=True)
